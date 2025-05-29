f18b3b9f392b366f1d21ec2790b5c36904616b45
aidandonn2 <aswilliams31@cps.edu>
2025-05-29 14:00:46 -0500

    Update 37 rock.py

--- 37 rock.py
+++ 37 rock.py
@@ -2,21 +2,21 @@
 #1/6/25
 #rockpapsci
 #init
-import random
+import random #Imports random so the computer can pick an option
 
 #func
 def rockpapsci():
     print("Welcome to Rock Paper Scissors")
     print("Select rock, paper or scissors")
-    player = input("Selection: ")
+    player = input("Selection: ") #This is where the user makes their choice
     computer = random.randint(1,3)
-    if computer == 1:
+    if computer == 1: #Rock is assigned to the number 1
         computer = "rock"
-    elif computer == 2:
+    elif computer == 2: #paper is assigned to the number 2
         computer = "paper"
-    elif computer == 3:
+    elif computer == 3: #scissor is assigned to the number 3
         computer = "scissor"
-    if computer == "scissor" and player == "paper":
+    if computer == "scissor" and player == "paper": #These codes show how the user will win
         print("You lose!")
     if computer == "rock" and player == "scissor":
         print("You lose!")
@@ -39,6 +39,6 @@ def rockpapsci():
     
         
 #main
-rockpapsci()
+rockpapsci() #The function
     
         
